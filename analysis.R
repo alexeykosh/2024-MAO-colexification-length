a24bbf92766884e40f93a76202e31d7c2ce9bebb
alexeykosh <alexeykochevoy@gmail.com>
2024-01-29 13:51:21 +0100

    analyses and data

---
+++ analysis.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+library(lme4)
+
+theme_set(theme_bw())
+
+# Inverse logit function
+inverse_logit <- function(x){
+  exp(x)/(1+exp(x))
+}
+
+lexibank_all <- read.csv('data/forms_total.csv')
+
+lexibank_all$Concepticon_ID <- as.numeric(lexibank_all$Concepticon_ID)
+
+lexibank_all_colex <- lexibank_all %>%
+  select(Glottocode, Form, 
+         Concepticon_ID, Length, 
+         Family) %>%
+  group_by(Glottocode, Form) %>%
+  mutate(colexification = n_distinct(Concepticon_ID)) %>%
+  group_by(Glottocode) %>%
+  mutate(n_forms = n()) %>%
+  distinct(Form, .keep_all = TRUE) %>%
+  mutate(colexification_bin = ifelse(colexification > 1, 1, 0))
+  
+
+logistic_data <- lexibank_all_colex %>%
+  transform(shuffle_colexification_bin = sample(colexification_bin)) %>%
+  group_by(Glottocode) %>%
+  mutate(z_length = (Length - mean(Length)) / sd(Length))  %>%
+  ungroup()  %>%
+  mutate(z_n_forms = (n_forms - mean(n_forms)) / sd(n_forms))
+
+model_shuffle_null <- glmer(shuffle_colexification_bin ~ 1 + z_n_forms 
+                            + (1 | Family / Glottocode), 
+                       family = "binomial",
+                       data = logistic_data)
+summary(model_shuffle_null)
+
+model_shuffle <- glmer(shuffle_colexification_bin ~ 1 + z_length + z_n_forms 
+                       + (1 + z_length | Family / Glottocode), 
+     family = "binomial",
+     data = logistic_data)
+summary(model_shuffle)
