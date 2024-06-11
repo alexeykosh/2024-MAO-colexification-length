64325aaf1dc93ac313e37c77c8085988526ba488
alexeykosh <alexeykochevoy@gmail.com>
2024-06-12 00:14:28 +0200

    modified the unzip procedure

--- preprocessing.py
+++ preprocessing.py
@@ -1,11 +1,16 @@
 import pandas as pd
+import zipfile
+import os
 
-# unzip the lexibank-analysed-v1.0.zip file in the data folder
-!unzip -o data/lexibank-analysed-v1.0.zip -d data/
-# unzip the forms.csv file in the cldf folder
-!unzip -o data/lexibank-lexibank-analysed-a4c0952/cldf/forms.csv.zip -d data/lexibank-lexibank-analysed-a4c0952/cldf/
+# Unzip the lexibank-analysed-v1.0.zip file in the data folder
+with zipfile.ZipFile('data/lexibank-analysed-v1.0.zip', 'r') as zip_ref:
+    zip_ref.extractall('data/')
 
-# load the forms, languages, and concepts csv files
+# Unzip the forms.csv file in the cldf folder
+with zipfile.ZipFile('data/lexibank-lexibank-analysed-a4c0952/cldf/forms.csv.zip', 'r') as zip_ref:
+    zip_ref.extractall('data/lexibank-lexibank-analysed-a4c0952/cldf/')
+
+# Load the forms, languages, and concepts csv files
 forms = pd.read_csv('data/lexibank-lexibank-analysed-a4c0952/cldf/forms.csv')
 languages = pd.read_csv('data/lexibank-lexibank-analysed-a4c0952/cldf/languages.csv')
 concepts = pd.read_csv('data/lexibank-lexibank-analysed-a4c0952/cldf/concepts.csv')
@@ -25,4 +30,5 @@ forms['Number_of_meanings'] = forms\
     .groupby(['Form', 'Language_ID'])['Concepticon_ID']\
     .transform('nunique')
 
+print('Number of forms:', forms.shape[0])
 forms.to_csv('data/forms_total.csv', index=False)
